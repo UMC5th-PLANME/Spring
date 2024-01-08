d09c3f6aaccc82a8f590c2f39f39a774093d675d
IkJuLim <limik1004@gmail.com>
2024-01-08 16:53:15 +0900

    first commit

---
+++ src/main/java/com/planme/main/MainServerApplication.java
@@ -0,0 +1,13 @@
+package com.planme.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planme/main/MainServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planme.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
