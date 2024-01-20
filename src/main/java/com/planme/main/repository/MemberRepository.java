9ec994b4fd0c92496b6952c19c8f1f57aa685e26
sonhoyoung <thsgdud25@naver.com>
2024-01-20 10:57:43 +0900

    Chore :  memberRepository 생성

---
+++ src/main/java/com/planme/main/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.planme.main.repository;
+
+import com.planme.main.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
