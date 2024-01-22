45c2bf7b5046e6adb7af5d50a127918ebd985374
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-01-22 17:03:46 +0900

    chore: MakeInitData에 스케줄 추가

--- src/main/java/com/planme/main/MakeInitData.java
+++ src/main/java/com/planme/main/MakeInitData.java
@@ -2,11 +2,17 @@ package com.planme.main;
 
 import com.planme.main.domain.Member;
 import com.planme.main.domain.Category;
+import com.planme.main.domain.Schedule;
+import com.planme.main.domain.enums.Repeat;
 import com.planme.main.repository.MemberRepository;
 import com.planme.main.repository.CategoryRepository;
+import com.planme.main.repository.ScheduleRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Component
 @RequiredArgsConstructor
@@ -14,9 +20,10 @@ public class MakeInitData {
 
     private final MemberRepository memberRepository;
     private final CategoryRepository categoryRepository;
+    private final ScheduleRepository scheduleRepository;
 
     @PostConstruct
-    public void makeTestMemberAndCategory() {
+    public void makeTestMemberAndCategoryAndSchedule() {
         Member member1 = Member.builder()
                 .nickname("testMember1")
                 .email("test@test.com")
@@ -33,6 +40,21 @@ public class MakeInitData {
                 .member(memberRepository.findByNickname("testMember1"))
                 .build();
         categoryRepository.save(category1);
+
+        Schedule schedule1 = Schedule.builder()
+                .title("Test Schedule")
+                .status(false)
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now())
+                .startTime(LocalTime.of(10, 0))
+                .endTime(LocalTime.of(12, 0))
+                .alarm(true)
+                .alarmTime(LocalTime.of(9, 30))
+                .repeatPeriod(Repeat.NONE)
+                .repeatDetails("")
+                .category(category1)
+                .build();
+        scheduleRepository.save(schedule1);
     }
 
 }
