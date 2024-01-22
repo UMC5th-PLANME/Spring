aa535519656de3e5fa94176ab13e41739ca31241
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-01-22 17:05:30 +0900

    feat: handling 관련 코드 추가

---
+++ src/main/java/com/planme/main/apiPayload/exception/handler/MemberHandler.java
@@ -0,0 +1,10 @@
+package com.planme.main.apiPayload.exception.handler;
+
+import com.planme.main.apiPayload.code.BaseErrorCode;
+import com.planme.main.apiPayload.exception.GeneralException;
+
+public class MemberHandler extends GeneralException {
+    public MemberHandler (BaseErrorCode errorCode){
+        super(errorCode);
+    }
+}
