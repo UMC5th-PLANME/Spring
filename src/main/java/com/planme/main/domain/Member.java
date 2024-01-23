96701aa393de8b9353b1b53c72ab5ecb03c62a6b
thsghdud13 <thsgdud25@naver.com>
2024-01-23 14:45:36 +0900

    fix : Member DB에 profile image 저장할 수 있게 수정

--- src/main/java/com/planme/main/domain/Member.java
+++ src/main/java/com/planme/main/domain/Member.java
@@ -3,6 +3,8 @@ package com.planme.main.domain;
 import com.planme.main.domain.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,12 +28,12 @@ public class Member extends BaseEntity {
 
     private String email;
 
+    private String profileImage;
+
     @Column(columnDefinition = "TINYINT(1)")
     private boolean status; //  0: 비활성화, 1: 활성
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<Category> categoryList = new ArrayList<>();
-
-
 
 }
