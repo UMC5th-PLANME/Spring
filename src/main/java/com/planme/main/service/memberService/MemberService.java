5b50f1c0fb3bd61f38a817f09198f54860d78baa
thsghdud13 <thsgdud25@naver.com>
2024-01-24 02:07:26 +0900

    feat : 회원 조회 컨트롤러 구현

--- src/main/java/com/planme/main/service/memberService/MemberService.java
+++ src/main/java/com/planme/main/service/memberService/MemberService.java
@@ -2,7 +2,9 @@ package com.planme.main.service.memberService;
 
 import com.planme.main.domain.Member;
 import com.planme.main.oauth2.user.ProviderUser;
+import jakarta.servlet.http.HttpServletRequest;
 
 public interface MemberService {
     public Member join(String registrationId, ProviderUser providerUser);
+    public Member getMember(HttpServletRequest httpServletRequest);
 }
