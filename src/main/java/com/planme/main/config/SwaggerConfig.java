bce3a18230f32a773e0a3cab2025212f9b74a56b
yseo14 <lmslys14@naver.com>
2024-01-25 04:18:34 +0900

    Feat: Swagger 세팅
    API 명세를 위한 swagger 세팅
    
    Related: #18

---
+++ src/main/java/com/planme/main/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.planme.main.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+
+    @Bean
+    public OpenAPI openAPI() {
+
+            Info info = new Info()
+                    .title("PLAN ME API")
+                    .description("PLAN ME API 명세서")
+                    .version("1.0.0");
+
+        String jwtSchemeName = "JWT TOKEN";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
