636c4a6bac3c1db78b3b9757025bf67acf5128f5
thsghdud13 <thsgdud25@naver.com>
2024-01-24 22:55:03 +0900

    Chore : 약관 관련 예외처리

--- src/main/java/com/planme/main/apiPayload/code/status/SuccessStatus.java
+++ src/main/java/com/planme/main/apiPayload/code/status/SuccessStatus.java
@@ -15,6 +15,7 @@ public enum SuccessStatus implements BaseCode {
 
     // 멤버 관련 응답
     MEMBER_FOUND(HttpStatus.OK,"MEMBER2001", "회원을 조회했습니다."),
+    MEMBER_TERMS_AGREED(HttpStatus.OK, "MEMBER2002", "회원의 이용약관 동의했습니다."),
 
     // 카테고리 관련 응답
     CATEGORY_CREATED(HttpStatus.OK,"CATEGORY2001","카테고리가 추가되었습니다."),
