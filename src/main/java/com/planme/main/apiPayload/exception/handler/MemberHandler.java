2437dbeb7e27b14a61cf9403da6fd3f60d9bfc51
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-01-25 15:30:02 +0900

    chore: MemeberHandler 충돌 해결

--- src/main/java/com/planme/main/apiPayload/exception/handler/MemberHandler.java
+++ src/main/java/com/planme/main/apiPayload/exception/handler/MemberHandler.java
@@ -4,12 +4,7 @@ import com.planme.main.apiPayload.code.BaseErrorCode;
 import com.planme.main.apiPayload.exception.GeneralException;
 
 public class MemberHandler extends GeneralException {
-<<<<<<< HEAD
-    public MemberHandler (BaseErrorCode errorCode){
+    public MemberHandler(BaseErrorCode errorCode) {
         super(errorCode);
-=======
-    public MemberHandler(BaseErrorCode code) {
-        super(code);
->>>>>>> origin/dev
     }
 }
