47d968cea67316456ea53967fbe1fa92d8ac7811
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-01-26 14:38:52 +0900

    chore: ScheduleSuccessStatus 추가 및 수정

--- src/main/java/com/planme/main/apiPayload/code/status/SuccessStatus.java
+++ src/main/java/com/planme/main/apiPayload/code/status/SuccessStatus.java
@@ -18,8 +18,9 @@ public enum SuccessStatus implements BaseCode {
     MEMBER_TERMS_AGREED(HttpStatus.OK, "MEMBER2002", "회원의 이용약관 동의했습니다."),
 
     // 스케줄 관련 응답
-    SCHEDULE_FOUND(HttpStatus.OK, "SCHEDULE2005", "스케줄 조회 성공입니다."),
-    SCHEDULE_DELETE(HttpStatus.OK, "SCHEDULE2006", "스케줄 삭제 성공입니다."),
+    SCHEDULE_FOUND(HttpStatus.OK, "SCHEDULE2002", "스케줄 조회 성공입니다."),
+    SCHEDULE_UPDATE(HttpStatus.OK, "SCHEDULE2003", "스케줄 수정 성공입니다."),
+    SCHEDULE_DELETE(HttpStatus.OK, "SCHEDULE2004", "스케줄 삭제 성공입니다."),
 
     // 카테고리 관련 응답
     CATEGORY_CREATED(HttpStatus.OK,"CATEGORY2001","카테고리가 추가되었습니다."),
