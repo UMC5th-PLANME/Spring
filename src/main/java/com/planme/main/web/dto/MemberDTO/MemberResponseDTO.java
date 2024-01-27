12215d6b4c1df037f260590acc44ecf5d224ee17
thsghdud13 <thsgdud25@naver.com>
2024-01-27 15:46:21 +0900

    Chore : MemberDTO 작성

--- src/main/java/com/planme/main/web/dto/MemberDTO/MemberResponseDTO.java
+++ src/main/java/com/planme/main/web/dto/MemberDTO/MemberResponseDTO.java
@@ -1,4 +1,22 @@
 package com.planme.main.web.dto.MemberDTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 public class MemberResponseDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UpdateProfileDTO{
+        private Long member_id;
+        private String name;
+        private String image_url;
+        private LocalDateTime updated_at;
+    }
 }
